e56611b9e6592b3a2bdb498632b2a40d0b57c53e
Philipp Giese <philipp.giese@auteon.de>
2021-02-15 14:15:09 +0100

    feat: warn when out-of-scope variables are referenced in $eval callbacks

--- tests/lib/rules/no-external-eval.js
+++ tests/lib/rules/no-external-eval.js
@@ -2,36 +2,81 @@
  * @fileoverview Checks whether eval callbacks only access variables from their defined scope.
  * @author Philipp Giese
  */
-"use strict";
 
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
 
-var rule = require("../../../lib/rules/no-external-eval"),
-
-    RuleTester = require("eslint").RuleTester;
-
+const rule = require('../../../lib/rules/no-external-eval')
+const { RuleTester } = require('eslint')
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester();
-ruleTester.run("no-external-eval", rule, {
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2015 } })
+ruleTester.run('no-external-eval', rule, {
+  valid: [
+    {
+      code: `
+          function test () { 
+              page.$eval('.button', (button) => {
+                  if(button instanceof HTMLButtonElement) {
+                      return
+                  }
+              })
+          }
+              `,
+    },
+    {
+      code: `
+          function test () { 
+              const outside = 'test'
 
-    valid: [
+              page.$eval('.button', (_, outside) => {
+                  if(button instanceof HTMLButtonElement) {
+                      return
+                  }
+              }, outside)
+          }
+              `,
+    },
+  ],
 
-        // give me some code that won't trigger a warning
-    ],
+  invalid: [
+    {
+      code: `
+        function test () {
+            const outside = 'test'
 
-    invalid: [
-        {
-            code: "",
-            errors: [{
-                message: "Fill me in.",
-                type: "Me too"
-            }]
+            page.$eval('.selector', () => {
+                outside
+            })
         }
-    ]
-});
+            `,
+      errors: [
+        {
+          message:
+            'The variable "outside" is defined outside the scope of the $eval method.',
+        },
+      ],
+    },
+    {
+      code: `
+        function test () {
+            const outside = 'test'
+
+            page.$eval('.selector', function() {
+                outside
+            })
+        }
+            `,
+      errors: [
+        {
+          message:
+            'The variable "outside" is defined outside the scope of the $eval method.',
+        },
+      ],
+    },
+  ],
+})
